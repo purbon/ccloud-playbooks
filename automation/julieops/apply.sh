38dbe3d9b0981ae317b45d0e91ebd76b4524a171
Pere Urbon <pere@confluent.io>
2022-04-27 18:33:54 +0200

    add example of using julieops for automation

---
+++ automation/julieops/apply.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+java -jar julie-ops.jar --brokers pkc-ldvmy.centralus.azure.confluent.cloud:9092 \
+                        --clientConfig config.properties \
+                        --topology topologies/descriptor.yml
