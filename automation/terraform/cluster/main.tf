2bdb690b19320fbf3aec370f22a00fe467b21118
Pere Urbon <pere@confluent.io>
2022-08-30 11:43:03 +0200

    update the terraform example for latest version

--- automation/terraform/cluster/main.tf
+++ automation/terraform/cluster/main.tf
@@ -1,26 +1,59 @@
 terraform {
+  required_version = ">= 0.14.0"
   required_providers {
-    confluentcloud = {
-      source  = "confluentinc/confluentcloud"
-      version = "0.5.0"
+    confluent = {
+      source  = "confluentinc/confluent"
+      version = "1.2.0"
     }
   }
 }
 
-provider "confluentcloud" {}
 
-data "confluentcloud_environment" "purbon" {
-  id = "env-j9wgp"
+provider "confluent" {
+  cloud_api_key    = var.confluent_cloud_api_key
+  cloud_api_secret = var.confluent_cloud_api_secret
 }
 
-resource "confluentcloud_kafka_cluster" "basic-cluster-on-azure" {
-  display_name = "basic_kafka_cluster_on_azure"
-  availability = "SINGLE_ZONE"
-  cloud        = "AZURE"
-  region       = "centralus"
-  basic {}
+data "confluent_environment" "purbon" {
+  id = var.confluent_cloud_environment
+}
 
+resource "confluent_network" "private-link" {
+  display_name     = "Private Link Network"
+  cloud            = "AWS"
+  region           = var.region
+  connection_types = ["PRIVATELINK"]
+  zones            = keys(var.subnets_to_privatelink)
   environment {
-    id = data.confluentcloud_environment.purbon.id
+    id = data.confluent_environment.purbon.id
+  }
+}
+
+resource "confluent_private_link_access" "aws" {
+  display_name = "AWS Private Link Access"
+  aws {
+    account = var.aws_account_id
+  }
+  environment {
+    id = data.confluent_environment.purbon.id
+  }
+  network {
+    id = confluent_network.private-link.id
+  }
+}
+
+resource "confluent_kafka_cluster" "dedicated" {
+  display_name = "inventory"
+  availability = "MULTI_ZONE"
+  cloud        = confluent_network.private-link.cloud
+  region       = confluent_network.private-link.region
+  dedicated {
+    cku = 2
+  }
+  environment {
+    id = data.confluent_environment.purbon.id
+  }
+  network {
+    id = confluent_network.private-link.id
   }
 }
---
+++ automation/terraform/cluster/variables.tf
@@ -0,0 +1,25 @@
+variable "confluent_cloud_api_key" {
+  description = "Confluent Cloud API Key (also referred as Cloud API ID)"
+  type        = string
+}
+
+variable "confluent_cloud_api_secret" {
+  description = "Confluent Cloud API Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_account_id" {
+  description = "The AWS Account ID (12 digits)"
+  type        = string
+}
+
+variable "confluent_cloud_environment" {
+  description = "A confluent cloud environment label"
+  type        = string
+}
+
+variable "subnets_to_privatelink" {
+  description = "A map of Zone ID to Subnet ID (i.e.: {\"use1-az1\" = \"subnet-abcdef0123456789a\", ...})"
+  type        = map(string)
+}
